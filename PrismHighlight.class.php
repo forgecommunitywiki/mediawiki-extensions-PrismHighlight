45a7ac73fe412aaa6eb8a60ef2250eb8fca3d640
SciWhiz12 <arnoldnunag12@gmail.com>
2021-04-08 17:34:57 +0800

    Initial commit

---
+++ PrismHighlight.class.php
@@ -0,0 +1,69 @@
+<?php
+
+class PrismHighlight {
+
+    public static function onBeforePageDisplay(OutputPage &$out, Skin &$skin) {
+        $out->addModules('ext.PrismHighlight');
+        return true;
+    }
+
+    public static function onParserFirstCallInit(Parser &$parser) {
+        global $wgHighlightTags;
+
+        foreach ($wgHighlightTags as $tag) {
+            // $parser->setHook( tag, array( class, method ) );
+            $parser->setHook($tag, array('PrismHighlight', 'renderSyntaxhighlight'));
+		    }
+
+		    return true;
+	  }
+
+    public static function renderSyntaxhighlight($in, $param = array(), $parser = null, $frame = false) {
+        global $wgLangMapping;
+
+        // get the language
+        //<syntaxhighlight lang="bash">
+        //</syntaxhighlight>
+        $lang = isset($param['lang']) ? $param['lang'] : '';
+
+        $highlightClass = 'code2highlight';
+        if ($lang == 'nohighlight')
+        {
+            $highlightClass = 'nohighlight';
+            $lang = '';
+        }
+
+        // map lang if necessary
+        if (array_key_exists($lang, $wgLangMapping)) {
+            $lang = $wgLangMapping[$lang];
+        }
+
+        // class
+        $htmlAttribs['class'] = isset($param['class']) ? $param['class'] . ' ' . $highlightClass : $highlightClass;
+        if (!empty($lang)) {
+            $htmlAttribs['class'] .= " lang-$lang";
+        }
+        // id
+        if (isset( $param['id']))
+        {
+            $htmlAttribs['id'] = $param['id'];
+        }
+
+        $code = htmlspecialchars(trim($in));
+
+        // inline ?
+        //<syntaxhighlight lang="bash" inline></syntaxhighlight>
+        $inline = isset($param['inline']);
+
+        if ($inline) {
+            $htmlAttribs['style'] = 'display: inline;';
+            $out = Html::rawElement('code', $htmlAttribs, $code);
+            return $out;
+        }
+        else {
+            $out = Html::rawElement('pre', $htmlAttribs, $code);
+            return $out;
+        }
+    }
+
+}
---
+++ PrismHighlight.php
@@ -0,0 +1,8 @@
+<?php
+
+if ( function_exists( 'wfLoadExtension' ) ) {
+    wfLoadExtension( 'PrismHighlight' );
+    return true;
+} else {
+    die( 'This version of the PrismHighlight extension requires MediaWiki 1.35+' );
+}
