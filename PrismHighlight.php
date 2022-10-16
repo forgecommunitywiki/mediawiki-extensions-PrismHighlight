0a69677dfccf33492a8e4e9ce3df175e37435cee
Paint_Ninja <PaintNinja@users.noreply.github.com>
2022-10-16 14:47:59 +0100

    Document, optimise and update
    - Add more function type declarations
    - Add PHPDoc comments for better IDE support of ambiguous variable types
    - ES5 Strict Mode
    - Prism.js 1.29.0 with added Gradle support, removed PLSQL, JS and TS support

--- PrismHighlight.php
+++ PrismHighlight.php
@@ -1,8 +1,8 @@
 <?php
 
-if ( function_exists( 'wfLoadExtension' ) ) {
-    wfLoadExtension( 'PrismHighlight' );
+if (function_exists('wfLoadExtension')) {
+    wfLoadExtension('PrismHighlight');
     return true;
 } else {
-    die( 'This version of the PrismHighlight extension requires MediaWiki 1.35+' );
+    die('This version of the PrismHighlight extension requires MediaWiki 1.35+');
 }
